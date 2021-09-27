3e013bb1a6382aba2bd2d0870851c11658a6071c
raduanakhawla <raduanakhawla468@gmail.com>
2021-09-27 23:00:42 +0600

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import AlParticipant from './components/AllParticipants/AlParticipants';
 import Header from './components/Header';
-import AlParticipants from './components/AlParticipants/AlParticipants';
 
 function App() {
   return (
     <div>
     <Header></Header> 
-    <AlParticipants></AlParticipants>
+    <AlParticipant></AlParticipant>
     </div>
   );
 }
